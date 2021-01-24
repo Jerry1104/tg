00839536d744c2786597a53db61e700b358f013a
Jerry1104 <liujianguo1104@163.com>
2021-01-25 01:50:52 +0800

    up

--- tg_parent/tg_config/src/main/java/com/tg/controller/TestController.java
+++ tg_parent/tg_config/src/main/java/com/tg/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.tg.admin.controller;
+package com.tg.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
