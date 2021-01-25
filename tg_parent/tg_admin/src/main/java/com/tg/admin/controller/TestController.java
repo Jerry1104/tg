c7b8e6d18e178b4bc832800d62f8486398857c21
Jerry1104 <liujianguo1104@163.com>
2021-01-25 23:03:15 +0800

    yml log

--- tg_parent/tg_admin/src/main/java/com/tg/admin/controller/TestController.java
+++ tg_parent/tg_admin/src/main/java/com/tg/admin/controller/TestController.java
@@ -1,11 +1,20 @@
 package com.tg.admin.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+	@Value("boot.msg")
+	private  String msg;
+	
+	@GetMapping("/msg")
+    public  String msg(){
+        return msg;
+    }
+	
     @GetMapping("/home")
     public  String home(){
         return "home";
